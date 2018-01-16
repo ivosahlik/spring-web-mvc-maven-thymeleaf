9a42f89289f8ac395718d61168ac2ee1153add4e
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-01-16 22:44:50 +0100

    initial commit, create web.xml, dispatcher-servlet, controler

---
+++ src/main/java/cz/ivosahlik/controller/cz/ivosahlik/config/AppConfig.java
@@ -0,0 +1,18 @@
+package cz.ivosahlik.controller.cz.ivosahlik.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Intellij Idea
+ * Created by ivosahlik on 16/01/18
+ */
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+
+    
+
+
+}
